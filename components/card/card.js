3ed01805c7da21f7f5c26f40e42e605b44fcb9e9
Manu7864z <zropfmanuel@gmail.com>
2023-02-06 14:28:52 +0100

    fetch-the-data

--- components/card/card.js
+++ components/card/card.js
@@ -1,25 +1,25 @@
-export function createCharacterCard() {
+export function createCharacterCard(characterData) {
   const createCard = document.createElement("li");
   const cardContainerUl = document.querySelector('[data-js="card-container"]');
   createCard.innerHTML = `<div class="card__image-container">
   <img
     class="card__image"
-    src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-    alt="Rick Sanchez"
+    src="${characterData.image}" 
+    alt="${characterData.name}"
   />
   <div class="card__image-gradient"></div>
-</div>
-<div class="card__content">
-  <h2 class="card__title"></h2>
+  <div class="card__content">
+  <h2 class="card__title">${characterData.name}</h2>
   <dl class="card__info">
-    <dt class="card__info-title"></dt>
-    <dd class="card__info-description"></dd>
+    <dt class="card__info-title">Status</dt>
+    <dd class="card__info-description">${characterData.status}</dd>
     <dt class="card__info-title">Type</dt>
-    <dd class="card__info-description"></dd>
-    <dt class="card__info-title"></dt>
-    <dd class="card__info-description"></dd>
+    <dd class="card__info-description">${characterData.type}</dd>
+    <dt class="card__info-title">Occurrences</dt>
+    <dd class="card__info-description">${characterData.occurrences}</dd>
   </dl>
 </div>`;
+createCard.classList.add("card");
 
-  cardContainerUl.append("createCard");
+  cardContainerUl.append(createCard);
 }
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import { createCharacterCard } from "./components/card/card.js";
+
+/* fetch('https://rickandmortyapi.com/api/character').then(response => { 
+  if (response.ok) { 
+    return response.json(); 
+  }
+  throw new Error("request failed") 
+
+}, networkerror => console.log(networkerror.message).then(jsonResponse => { 
+
+})
+);  */
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +25,35 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+  async function fetchCharacters() { 
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await response.json();
+    return data;
+  }
+
+  fetchCharacters().then((data) => {
+    data.results.forEach((characterData) => {
+      createCharacterCard(characterData);
+    });
+  });
+  
+
+
+/*   createCharacterCard(fetchCharacters);  */
+  
+/*   const characters = fetchCharacters(); 
+  console.log(characters);   */
+
+  /*  characters.array.forEach(async (character) => {
+    const dataChar = await fetchCharacters; 
+    
+   }); */
+
+  /* Object.keys(characters).forEach(key => {
+    console.log(key, characters[key]); 
+
+  })  */
+ 
+
+
